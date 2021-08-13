208cdec0ec26614b58d1eb1a53de0836feeb3b92
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-13 16:21:46 +0300

    feat: add onClickHandler(next,back)

--- authorization/auth.js
+++ authorization/auth.js
@@ -1,55 +1,74 @@
-function LogIn() {
+let successfullyAccess = false
+
+
+ async function LogIn() {
+    
+    let authForm = document.getElementById('form')
     
     let userPassword = document.getElementById('pass')
     let userEmail = document.getElementById('email')
     let validation = validator(userEmail.value, userPassword.value)
     
-    if (validation) {
+      if (validation) {
         let userData = {
             email: userEmail.value,
             password: userPassword.value
         }
-        let userJsonDate = JSON.stringify(userData)
         
-        authorization(userJsonDate)
+        let userJsonDate = JSON.stringify(userData)
+      successfullyAccess= await authorization(userJsonDate)
+        if (successfullyAccess) {
+            authForm.classList.add('hidden')
+
+            let script = document.createElement('script')
+            script.src = "gallery/get_gallery.js"
+            document.getElementsByTagName('head')[0].appendChild(script);
+
+        } else {
+            authForm.classList.add('visible')
+        }
     } else {
         alert('некорректные данные')
     }
-    
+   
     setTimeout(removeToken, 600000)
+    
 }
 
-function authorization(userJsonDate) {
-    let result
-    
-    result = fetch('https://glq7fjiy07.execute-api.us-east-1.amazonaws.com/api/login', {
+  function authorization(userJsonDate) {
+   
+    let start =  fetch('https://glq7fjiy07.execute-api.us-east-1.amazonaws.com/api/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: userJsonDate
     })
-    result.then(data => {
+   return   start.then(data => {
         if (data.status === 200) {
-            data.json().then(data => {
+           return  data.json().then(data => {
                 saveToken(data)
+                doSome(true)
+                return true
             })
         } else {
             alert(`Error, status:${data.status}`)
+            doSome(false)
+    return false
         }
         
     }) //add token in localStorage
-    
-    
+      
+   
 }
-
+function doSome(data){someResult=data}
 function saveToken(token) {
     localStorage.setItem('tokenData', JSON.stringify(token));
 }
 
 function validator(email, password) {
-    let regexp = /^........*$/i
-    
+    let regexp = /^........*$/i // /[\w|\d]+|\d+
+    //let regexp = /^\w+@flo.team$/
     if (regexp.test(email) && regexp.test(password)) {
         return true
     } else {
@@ -59,6 +78,9 @@ function validator(email, password) {
 }
 
 function removeToken() {
+    let authForm = document.getElementById('form')
+    authForm.classList.add('visible')
     localStorage.removeItem('tokenData')
     
 }
+
--- gallery/get_gallery.js
+++ gallery/get_gallery.js
@@ -1,7 +1,25 @@
+let page = null
+localStorage.getItem('page') ? page = localStorage.getItem('page') : page = 1
+let url = `https://glq7fjiy07.execute-api.us-east-1.amazonaws.com/api/gallery?page=${page}`
+
+function get_page() {
+    localStorage.getItem('page') ? page = localStorage.getItem('page') : page = 1
+    return page
+}
+
+function set_page(num) {
+    
+    localStorage.setItem('page', num)
+    page = num
+    url = `https://glq7fjiy07.execute-api.us-east-1.amazonaws.com/api/gallery?page=${page}`
+    
+}
+
+
 async function get_gallery() {
     let token = localStorage.getItem('tokenData')
     token = JSON.parse(token)
-    let result = fetch('https://glq7fjiy07.execute-api.us-east-1.amazonaws.com/api/gallery?page=3', {
+    let result = fetch(url, {
         method: "GET",
         headers: {
             'Authorization': token.token
@@ -15,17 +33,42 @@ async function get_gallery() {
     create_img_elements()
     
     function create_img_elements() {
-        let divGallery=document.getElementById('gallery')
+        let divGallery = document.getElementById('gallery')
+        
+        while (divGallery.firstChild) {
+            divGallery.removeChild(divGallery.firstChild);
+        }
+        
+        
         for (let url of galleryObject.objects) {
             let img = document.createElement('img')
             img.src = url
             divGallery.appendChild(img)
             
-            
         }
         
     }
     
+}
+
+function onClickNext() {
+    let page1 = Number(get_page())
+    if (page >= 5) {
+        set_page(5)
+    } else {
+        set_page(page1 + 1)
+        get_gallery()
+    }
+}
+
+function onClickBack() {
+    let page1 = Number(get_page())
+    if (page == 1) {
+        set_page(1)
+    } else {
+        set_page(page1 - 1)
+        get_gallery()
+    }
 }
 
 get_gallery()
